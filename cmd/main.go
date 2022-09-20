822948ea8fcf2f7bacd6768185cae3ea2970e671
wf <shooterfei@163.com>
2022-09-20 21:48:38 +0800

    ref |> 分屏对比功能完成

--- cmd/main.go
+++ cmd/main.go
@@ -16,13 +16,25 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
+var baseIndex int
 func white_button(text string) *fyne.Container {
+	index := 0
 	var btn *widget.Button
 	var btn_color *canvas.Rectangle
 	var container1 *fyne.Container
 	btn = widget.NewButton(text, func() {
-		fmt.Println("Here is Go Button")
-		fmt.Println(btn_color.FillColor)
+		fmt.Println("---------------")
+		fmt.Println(index, baseIndex)
+		if index < baseIndex {
+			if index == 0 {
+				index = baseIndex
+				baseIndex++
+			}
+		} else {
+			baseIndex++
+		}
+		fmt.Println(index, baseIndex)
+		fmt.Println("---------------")
 		if btn_color.FillColor == color.White {
 			btn_color.FillColor = color.NRGBA{R: 123, G: 123, B: 123, A: 123}
 		} else {
@@ -39,6 +51,10 @@ func white_button(text string) *fyne.Container {
 }
 
 func main() {
+	var boxs *fyne.Container
+	baseIndex = 0
+	boxs = container.NewVBox()
+
 	font := "assets/fonts/STHeiti Light.ttc"
 
 	os.Setenv("FYNE_FONT", font)
@@ -46,7 +62,7 @@ func main() {
 	a := app.New()
 	window := a.NewWindow("操作票排序")
 
-	window.Resize(fyne.NewSize(1200, 800))
+	window.Resize(fyne.NewSize(1600, 900))
 
 	filesView := widget.NewList(
 		func() int {
@@ -54,9 +70,6 @@ func main() {
 		},
 		func() fyne.CanvasObject {
 			return widget.NewLabel("template")
-			// return widget.NewButton("test", func() {
-			// 	fmt.Println("test")
-			// })
 		},
 		func(id widget.ListItemID, object fyne.CanvasObject) {
 			object.(*widget.Label).SetText(resource10kVTxt[id].StaticName)
@@ -64,46 +77,50 @@ func main() {
 
 		})
 
-	contentText := widget.NewLabel("")
+	// contentText := widget.NewLabel("")
 
-	var ticket []string
+	// var ticket []string
 	// contentText := widget.NewButton("test", nil)
+	//
+
+	right := container.NewVBox()
 	filesView.OnSelected = func(id widget.ListItemID) {
-		ticket = strings.Split(string(resource10kVTxt[id].StaticContent), "\n")
+		baseIndex = 0
+		boxs.RemoveAll()
+		right.RemoveAll()
+
+		// sources.SetText(string(resource10kVTxt[id].StaticContent))
+		sources := strings.Split(string(resource10kVTxt[id].StaticContent), "\n")
+		ticket := strings.Split(string(resource10kVTxt[id].StaticContent), "\n")
 		r := rand.New(rand.NewSource(time.Now().UnixNano()))
 		r.Shuffle(len(ticket), func(i, j int) {
 			ticket[i], ticket[j] = ticket[j], ticket[i]
 		})
 
-		context := strings.Join(ticket, "\n")
-		contentText.SetText(context)
+		for i, v := range ticket {
+			boxs.Add(white_button(v))
+			right.Add(white_button(fmt.Sprintf("%d. %s", i + 1, sources[i])))
+		}
+
+		// context := strings.Join(ticket, "\n")
+		// contentText.SetText(context)
 
 	}
 
 	filesView.Select(0)
-	var boxs *fyne.Container
 
-	boxs = container.NewVBox()
-	for _, v := range ticket {
-		boxs.Add(white_button(v))
-	}
+
+
+	check := container.NewHSplit(boxs, right)
 
 	split := container.NewHSplit(
 		filesView,
-		// container.NewMax(contentText),
-		boxs,
+		check,
 	)
 
 	split.Offset = 0.2
 
-	// window.SetContent(container.NewVBox(
-	// 	red_button(),
-	// 	red_button(),
-	// 	red_button(),
-	// 	red_button(),
-	// 	red_button(),
-	// 	red_button(),
-	// ))
+
 	window.SetContent(split)
 	window.ShowAndRun()
 
